bac6e95960f23ff885dfe54422d6304f2bb489a2
Wataru NAKAI <watnk0806@gmail.com>
2018-07-19 22:48:29 +0900

    Add config: datesep

--- src/extension.ts
+++ src/extension.ts
@@ -3,20 +3,21 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as os from 'os';
 
-const date = () => {
+const date = (sep: string) => {
     const now = new Date();
-    return [now.getFullYear(), now.getMonth() + 1, now.getDate()].map(d => d.toString().length > 1 ? d : `0${d}`).join('');
+    return [now.getFullYear(), now.getMonth() + 1, now.getDate()].map(d => d.toString().length > 1 ? d : `0${d}`).join(sep);
 };
 
 const expandHomeDir = (path: string): string => {
     return path.replace(/^~/, os.homedir);
-}
+};
 
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('daily-memo.open', async () => {
         const config = vscode.workspace.getConfiguration('daily-memo');
         const dir = config.get('directory');
-        const path = expandHomeDir(`${dir}/${date()}.md`);
+        const sep = config.get('datesep') as string;
+        const path = expandHomeDir(`${dir}/${date(sep)}.md`);
         if (!fs.existsSync(path)) {
             fs.closeSync(fs.openSync(path, 'w+'));
         }
